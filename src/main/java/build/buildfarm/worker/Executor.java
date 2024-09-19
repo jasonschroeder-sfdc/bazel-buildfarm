b614ef3e0c0a4d94c442aac3dd5fe184e4b0b863
Jason Schroeder <jschroeder@salesforce.com>
2024-09-19 13:08:13 -0700

    feat: support cgroups v2 and v1
    This adds support for CGroups v2 and maintains support for CGroups v1
    for one more release.
    
    Efforts made to keep v1 and v2 separate so that v1 support can be
    removed without a ton more refactoring.

--- src/main/java/build/buildfarm/worker/Executor.java
+++ src/main/java/build/buildfarm/worker/Executor.java
@@ -524,6 +524,7 @@ class Executor {
     }
     long startNanoTime = System.nanoTime();
     Process process;
+    log.log(Level.FINE, "executing " + processBuilder);
     try {
       process = ProcessUtils.threadSafeStart(processBuilder);
       process.getOutputStream().close();
