50201a5d19e609c09e82fa48de89aa0293c8a994
Jason Schroeder <jschroeder@salesforce.com>
2025-10-03 16:47:50 -0700

    perf(CASFileCache): WithSpan for startup indexing

--- src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
+++ src/main/java/build/buildfarm/cas/cfc/CASFileCache.java
@@ -93,6 +93,7 @@ import io.grpc.StatusException;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.ServerCallStreamObserver;
 import io.netty.handler.codec.http.QueryStringDecoder;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.Histogram;
@@ -1313,6 +1314,7 @@ public abstract class CASFileCache implements ContentAddressableStorage {
 
   private record FileEntryKey(String key, long size, boolean isExecutable, Digest digest) {}
 
+  @WithSpan
   public void initializeRootDirectory() throws IOException {
     for (Path dir : entryPathStrategy) {
       Files.createDirectories(dir);
@@ -1422,6 +1424,7 @@ public abstract class CASFileCache implements ContentAddressableStorage {
     prometheusMetricsThread.start();
   }
 
+  @WithSpan
   protected CacheLoadResults loadCache(
       Consumer<Digest> onStartPut, ExecutorService removeDirectoryService)
       throws IOException, InterruptedException {
@@ -1479,6 +1482,7 @@ public abstract class CASFileCache implements ContentAddressableStorage {
     return !name.matches("[0-9a-f]{2}");
   }
 
+  @WithSpan
   private CacheScanResults scanRoot(Consumer<Digest> onStartPut)
       throws IOException, InterruptedException {
     // create thread pool
