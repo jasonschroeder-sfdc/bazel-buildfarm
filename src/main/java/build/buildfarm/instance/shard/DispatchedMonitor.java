57f9c1bafac7dec6dcd7951d5b15723cbe849cb8
Jason Schroeder <jschroeder@salesforce.com>
2026-02-14 15:02:08 -0800

    perf: withSpan and SpanAttributes

--- src/main/java/build/buildfarm/instance/shard/DispatchedMonitor.java
+++ src/main/java/build/buildfarm/instance/shard/DispatchedMonitor.java
@@ -25,6 +25,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.google.protobuf.Duration;
+import io.opentelemetry.instrumentation.annotations.SpanAttribute;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -84,7 +86,9 @@ class DispatchedMonitor implements Runnable {
    * @param now the current timestamp in milliseconds used for calculating overdue duration
    * @return a listenable future that completes when the requeue operation finishes
    */
-  private ListenableFuture<Void> requeueDispatchedExecution(DispatchedOperation o, long now) {
+  @WithSpan
+  private ListenableFuture<Void> requeueDispatchedExecution(
+      @SpanAttribute DispatchedOperation o, long now) {
     QueueEntry queueEntry = o.getQueueEntry();
     String operationName = queueEntry.getExecuteEntry().getOperationName();
 
@@ -111,7 +115,8 @@ class DispatchedMonitor implements Runnable {
    * @param o the overdue dispatched operation
    * @param now the current timestamp in milliseconds
    */
-  private void logOverdueOperation(DispatchedOperation o, long now) {
+  @WithSpan
+  private void logOverdueOperation(@SpanAttribute DispatchedOperation o, long now) {
     // log that the dispatched operation is overdue in order to indicate that it should be requeued.
     String operationName = o.getQueueEntry().getExecuteEntry().getOperationName();
     long overdue_amount = now - o.getRequeueAt();
@@ -156,6 +161,7 @@ class DispatchedMonitor implements Runnable {
    *     results (successful operations return null, failed operations return null due to
    *     successfulAsList behavior)
    */
+  @WithSpan
   private ListenableFuture<List<Void>> submitAll() {
     ImmutableList.Builder<ListenableFuture<Void>> requeuedFutures = ImmutableList.builder();
     try {
